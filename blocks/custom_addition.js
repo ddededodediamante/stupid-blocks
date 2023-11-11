2d6723408e47a66273b97a1a7932da1df2dd5a12
ddededodediamante <111385835+ddededodediamante@users.noreply.github.com>
2023-11-10 23:58:43 -0300

    Update custom_addition.js

--- blocks/custom_addition.js
+++ blocks/custom_addition.js
@@ -28,5 +28,5 @@ Blockly.JavaScript['custom_addition'] = function(block) {
   var value_num1 = Blockly.JavaScript.valueToCode(block, 'NUM1', Blockly.JavaScript.ORDER_ATOMIC);
   var value_num2 = Blockly.JavaScript.valueToCode(block, 'NUM2', Blockly.JavaScript.ORDER_ATOMIC);
   var code = value_num1 + ' + ' + value_num2;
-  return [code, Blockly.JavaScript.ORDER_NONE];
+  return [code, Blockly.JavaScript.ORDER_ADDITION];
 };
