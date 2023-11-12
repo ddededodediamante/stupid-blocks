ee0f78f347b56d1a35ffd4d03eb02fdc269c8b0c
ddededodediamante <111385835+ddededodediamante@users.noreply.github.com>
2023-11-12 04:01:12 -0300

    Create window_prompt.js

---
+++ blocks/window_prompt.js
@@ -0,0 +1,31 @@
+Blockly.Blocks['window_prompt'] = {
+  json: {
+  "type": "window_prompt",
+  "message0": "prompt %1",
+  "args0": [
+
+ {
+
+
+"type": "input_value",
+
+
+"name": "VALUE"
+
+ }
+  ],
+  "output": "String",
+  "colour": "#69717d",
+  "tooltip": "",
+  "helpUrl": ""
+},
+  init: function() {
+    this.jsonInit(this.json);
+  }
+};
+
+javascript.javascriptGenerator.forBlock['window_prompt'] = function(block, generator) {
+  var value_value = generator.valueToCode(block, 'VALUE', javascript.Order.ATOMIC);
+  var code = `window.prompt(${value_value})`;
+  return [code, javascript.order.NONE];
+};
