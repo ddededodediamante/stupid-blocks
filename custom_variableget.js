7110667dff6a71927840f2dad54dc14a7d4076fc
ddededodediamante <111385835+ddededodediamante@users.noreply.github.com>
2023-11-11 23:27:15 -0300

    Add files via upload

---
+++ custom_variableget.js
@@ -0,0 +1,27 @@
+Blockly.Blocks['custom_variableget'] = {
+  json: {
+    "type": "custom_variableget",
+    "message0": "get %1",
+    "args0": [
+      {
+        "type": "field_input",
+        "name": "VAR",
+        "text": "variable"
+      }
+    ],
+    "inputsInline": true,
+    "output": null,
+    "colour": 330,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  init: function() {
+    this.jsonInit(this.json);
+  }
+};
+
+javascript.javascriptGenerator.forBlock['custom_variableget'] = function(block, generator) {
+  var text_var = block.getFieldValue('VAR');
+  var code = `${text_var}`;
+  return [code, javascript.Order.NONE];
+};
