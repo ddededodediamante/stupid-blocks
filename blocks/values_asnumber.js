bd65d0a988a93dca052f7ed78b380249ec4889c0
ddededodediamante <111385835+ddededodediamante@users.noreply.github.com>
2023-11-13 17:26:35 -0300

    Create values_asnumber.js

---
+++ blocks/values_asnumber.js
@@ -0,0 +1,32 @@
+Blockly.Blocks['values_asnumber'] = {
+  json: {
+  "type": "values_asnumber",
+  "message0": "%1 as number",
+  "args0": [
+
+ {
+
+
+"type": "input_value",
+
+
+"name": "VALUE"
+
+ }
+  ],
+  "inputsInline": true,
+  "output": null,
+  "colour": 230,
+  "tooltip": "",
+  "helpUrl": ""
+},
+  init: function() {
+    this.jsonInit(this.json);
+  }
+};
+
+javascript.javascriptGenerator.forBlock['values_asnumber'] = function(block, generator) {
+  var value_value = generator.valueToCode(block, 'VALUE', javascript.Order.ATOMIC);
+  var code = `Number(value_value)`;
+  return [code, Blockly.javascript.ORDER_NONE];
+};
