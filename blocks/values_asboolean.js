1f6fa4d22abee84796a475c29f9b2a9dac213f1a
ddededodediamante <111385835+ddededodediamante@users.noreply.github.com>
2023-11-14 00:27:38 -0300

    Create values_asboolean.js

---
+++ blocks/values_asboolean.js
@@ -0,0 +1,32 @@
+Blockly.Blocks['values_asboolean'] = {
+  json: {
+  "type": "values_asboolean",
+  "message0": "%1 as boolean",
+  "args0": [
+
+ {
+
+
+"type": "input_value",
+
+
+"name": "VALUE"
+
+ }
+  ],
+  "inputsInline": true,
+  "output": null,
+  "colour": 135,
+  "tooltip": "",
+  "helpUrl": ""
+},
+  init: function() {
+    this.jsonInit(this.json);
+  }
+};
+
+javascript.javascriptGenerator.forBlock['values_asboolean'] = function(block, generator) {
+  var value_value = generator.valueToCode(block, 'VALUE', javascript.Order.ATOMIC);
+  var code = `Boolean(${value_value})`;
+  return [code, javascript.Order.NONE];
+};
