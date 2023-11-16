deda2530113106e904fbd0c262e77fe1f1827114
ddededodediamante <111385835+ddededodediamante@users.noreply.github.com>
2023-11-16 19:20:04 -0300

    Update window_console.js

--- blocks/window_console.js
+++ blocks/window_console.js
@@ -95,11 +95,11 @@ javascript.javascriptGenerator.forBlock['window_console'] = function(block, gene
   var dropdown_console = block.getFieldValue('CONSOLE');
   var value_text = generator.valueToCode(block, 'TEXT', javascript.Order.ATOMIC) || '';
   if (dropdown_console == 'log') {
-    var code = `console.log(${value_name});\n`;
+    var code = `console.log(${value_text});\n`;
   } else if (dropdown_console == 'warn') {
-    var code = `console.warn(${value_name});\n`;
+    var code = `console.warn(${value_text});\n`;
   } else if (dropdown_console == 'error') {
-    var code = `console.error(${value_name});\n`;
+    var code = `console.error(${value_text});\n`;
   }
   return code;
 };
